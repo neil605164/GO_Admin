6a9e7f87e842c6004679f8927a3757a2885ae0bc
neil605164 <neil605164work@gmail.com>
2018-09-09 17:43:54 +0800

    [ADD] DB Connect

--- conf/dbcon.go
+++ conf/dbcon.go
@@ -1,22 +1,4 @@
-package main
-
-// import (
-// 	"database/sql"
-// 	"fmt"
-// 	"log"
-// )
-
-// func init() {
-// 	// 顯示日期 ｜ 顯示時間（台灣） ｜ 顯示檔案名稱
-// 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-// }
-
-// func main() {
-// 	r := route.SetupRouter()
-
-// 	// Listen and Server in 0.0.0.0:8080
-// 	r.Run(":8080")
-// }
+package dbConn
 
 import (
 	"database/sql"
@@ -38,10 +20,10 @@ func checkError(err error) {
 	}
 }
 
-func main() {
+func DBConnect() {
 	// Initialize connection string.
 	var connectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
-	fmt.Println(connectionString)
+
 	// Initialize connection object.
 	db, err := sql.Open("mysql", connectionString)
 	checkError(err)
